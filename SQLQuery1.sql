2d7816ef1dd2956bedd015cc0b94fd06453ba29c
poxiaotianming <1270939049@qq.com>
2016-12-26 23:14:52 +0800

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,38 @@
+insert into medicine values ('绿药膏',1200,30);
+insert into medicine values ('慢支喘康速',1300,80);
+insert into medicine values ('扶他林',1500,60);
+insert into medicine values ('六味地黄丸',1100,40);
+insert into medicine values ('益肾丸',1300,66);
+insert into medicine values ('舒血宁',1900,47);
+insert into medicine values ('病毒灵',1200,35);
+insert into medicine values ('吗丁啉',1200,56);
+
+
+insert into medicine values ('感冒清',1569,96);
+insert into medicine values ('板蓝根',1563,63);
+insert into medicine values ('去痛片',1532,45);
+insert into medicine values ('扑热息痛',1600,20);
+insert into medicine values ('速效伤风胶囊',1700,70);
+insert into medicine values ('阿斯匹林',1200,30);
+insert into medicine values ('心嗽平',1600,28);
+insert into medicine values ('麻仁丸',1200,60);
+insert into medicine values ('六神丸',1700,68);
+
+insert into medicine values ('关节镇痛膏',1230,40);
+insert into medicine values ('牛黄解毒丸',1113,29);
+insert into medicine values ('黄连上清片',1050,74);
+insert into medicine values ('黄连素',1650,74);
+insert into medicine values ('可乐定片',1630,26);
+insert into medicine values ('维生素C片',1230,70);
+insert into medicine values ('清凉油',1600,70);
+insert into medicine values ('头孢拉定胶囊',1500,60);
+insert into medicine values ('复方新诺明片',1800,90);
+
+insert into medicine values ('尼可刹',1300,61);
+insert into medicine values ('多巴胺',1800,47);
+insert into medicine values ('西地兰',1600,74);
+insert into medicine values ('利多卡因',1700,36);
+insert into medicine values ('利血平',1009,80);
+insert into medicine values ('硝酸甘油',1500,60);
+insert into medicine values ('甘露醇',1500,80);
+insert into medicine values ('安定',1200,70);
